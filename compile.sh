7e387043f6b8940185a92db094775fd62fb8dd93
Mengchi Zhang <zhan2308@purdue.edu>
2021-05-19 11:34:04 -0400

    Add flto options
    Signed-off-by: Mengchi Zhang <zhan2308@purdue.edu>

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-hipcc -fgpu-rdc -c a.cpp -o a.o
-hipcc -fgpu-rdc -c vectoradd_hip.cpp -o vectoradd_hip.o
-hipcc -fgpu-rdc --hip-link -v -save-temps a.o vectoradd_hip.o -o vectoradd_hip.exe
+hipcc -fgpu-rdc -c -flto a.cpp -o a.o
+hipcc -fgpu-rdc -c -flto vectoradd_hip.cpp -o vectoradd_hip.o
+hipcc -fgpu-rdc --hip-link -flto -v -save-temps a.o vectoradd_hip.o -o vectoradd_hip.exe
